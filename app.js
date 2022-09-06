737914e45bc59a57f457465c2d77526ff9a7877e
francis omondi <francisomondi17@gmail.com>
2022-09-06 05:45:46 +0300

    restful apis, express and creating our express server

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const http = require("http")
+const express = require("express")
+
+
+const app = express()
+
+app.get("/", (req, res) => {
+    res.send("Hello francis!!!!")
+})
+app.get("/api/courses", (req, res) => {
+    res.send([1, 2, 3, 4, 5, 6])
+})
+
+const port = process.env.PORT || 4000
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
+
+module.exports = app
--- logger.js
+++
@@ -1,8 +0,0 @@
-var url = "http:/loggermaster.io/log"
-
-
-function log(message) {
-    console.log(message)
-}
-
-module.exports.log = log
