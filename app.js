f55304767b0e447d49373c6d89e85c4e73608885
francis omondi <francisomondi17@gmail.com>
2022-09-06 06:21:40 +0300

    handling a get request, getting a single course using the id

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require("express")
-
-
 const app = express()
+
+const courses = [{
+        id: 1,
+        name: "Chemestry"
+    },
+    {
+        id: 2,
+        name: "physics"
+    },
+    {
+        id: 3,
+        name: "history"
+    }
+]
 
 app.get("/", (req, res) => {
     res.send("Hello francis!!!!")
 })
 app.get("/api/courses", (req, res) => {
-    res.send([1, 2, 3, 4, 5, 6])
+    res.send(courses)
 })
 
 app.get("/api/courses/:id", (req, res) => {
-    res.send(req.params.id)
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send("course with this id was not found")
+    res.send(course)
+
 })
 
 
