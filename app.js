5815becd12714e00a205047e8a5c5921b5931749
francis omondi <francisomondi17@gmail.com>
2022-09-06 19:38:23 +0300

    updating a course

--- app.js
+++ app.js
@@ -21,38 +21,65 @@ const courses = [{
 app.get("/", (req, res) => {
     res.send("Hello francis!!!!")
 })
+
+//get all courses
 app.get("/api/courses", (req, res) => {
     res.send(courses)
 })
 
+//get single course
 app.get("/api/courses/:id", (req, res) => {
     const course = courses.find(c => c.id === parseInt(req.params.id))
-    if (!course) res.status(404).send(`The course with this is was not found`)
+    if (!course) res.status(404).send(`The course with this ID was not found`)
     res.send(course)
 
 })
 
+//add course
 app.post("/api/courses", (req, res) => {
-    const schema = Joi.object({
-        name: Joi.string().min(3).required()
-    })
 
-
-    const result = schema.validate(req.body);
-
-
-    if (result.error) {
-        return res.status(400).send(result.error.details[0].message)
+    const {
+        error
+    } = validateCourse(req.body)
+    if (error) {
+        return res.status(400).send(error.details[0].message)
     }
     const course = {
         id: courses.length + 1,
         name: req.body.name
     }
-
     courses.push(course)
     res.send(courses)
 })
 
+//update courses
+app.put("/api/courses/:id", (req, res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id))
+    if (!course) res.status(404).send(`The course with this is was not found`)
+
+
+    const {
+        error
+    } = validateCourse(req.body)
+    if (error) {
+        return res.status(400).send(error.details[0].message)
+    }
+    //update course
+    course.name = req.body.name
+    //return the updated course
+    res.send(courses)
+
+})
+
+
+
+function validateCourse(course) {
+    const schema = Joi.object({
+        name: Joi.string().min(3).required()
+    })
+
+    return schema.validate(course);
+}
 
 const port = process.env.PORT || 3000
 app.listen(port, () => {
