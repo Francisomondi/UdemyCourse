d752315669cb2058ee4450adedfb2eea11f39faf
francis omondi <francisomondi17@gmail.com>
2022-09-06 05:48:58 +0300

    getting a single item /api/courses/:id

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ app.get("/api/courses", (req, res) => {
     res.send([1, 2, 3, 4, 5, 6])
 })
 
-const port = process.env.PORT || 4000
+app.get("/api/courses/:id", (req, res) => {
+    res.send(req.params.id)
+})
+
+
+const port = process.env.PORT || 3000
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
